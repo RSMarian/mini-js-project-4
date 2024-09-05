36e266e145b36e8f96aa776c15b1f6c15b5471db
Marian Rusu <marianstefan1499@gmail.com>
2024-09-05 17:34:34 +0300

    fixed time format
    formated time as minutes:seconds then padded them with 0-s

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ let secondsElapsed = 0;
 let interval = null;
 const time = document.getElementById("time")
 
+function padStart(value) {
+    return String(value).padStart(2, "0")
+}
+
 function setTime() {
-    time.innerHTML = secondsElapsed
+    const minutes = Math.floor(secondsElapsed / 60)
+    const seconds = secondsElapsed % 60
+    time.innerHTML = `${padStart(minutes)}:${padStart(seconds)}`;
 }
 
 function timer() {
